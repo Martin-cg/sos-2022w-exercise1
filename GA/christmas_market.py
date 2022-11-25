34080a317e9c11fe05122acee5cd8827585d6361
Martin <e11909911@student.tuwien.ac.at>
2022-11-25 15:38:59 +0100

    Preparation for parallel processing

--- GA/christmas_market.py
+++ GA/christmas_market.py
@@ -1,4 +1,6 @@
+from multiprocessing import Pool
 from dataclasses import dataclass
+from typing import Tuple
 from sko.GA import GA_TSP
 from nptime import nptime as time
 from datetime import timedelta;
@@ -7,6 +9,7 @@ import os
 import humanfriendly
 import numpy as np
 import matplotlib.pyplot as plt
+from object_pool import ObjectPool
 
 TIME_PER_MARKET = timedelta(minutes=30).seconds
 NEED_FULL_STAY_AT_LAST_MARKET = False
@@ -49,125 +52,162 @@ def load_distance_matrix() -> pd.DataFrame:
     # df = df.applymap(lambda s: timedelta(seconds=s))
     return df
 
-# Load market list and distance matrix
-markets = np.array(load_markets())
-durations = np.array(load_distance_matrix(), dtype=np.int32)
+def main():
+    # Load market list and distance matrix
+    markets = np.array(load_markets())
+    durations = np.array(load_distance_matrix(), dtype=np.int32)
 
-name_loc = 0
-opening_loc = 1
-closing_loc = 2
+    name_loc = 0
+    opening_loc = 1
+    closing_loc = 2
 
-first_opening_time = min(markets[:, opening_loc])
-last_closing_time = max(markets[:, closing_loc])
+    first_opening_time = min(markets[:, opening_loc])
+    last_closing_time = max(markets[:, closing_loc])
 
-def christmas_market(p):
-    start_time = markets[p[0], opening_loc]
-    current_time = start_time
-    markets_visited = 0
+    def christmas_market(p) -> Tuple[int, int]:
+        start_time = markets[p[0], opening_loc]
+        current_time = start_time
+        markets_visited = 0
+        
+        # walk along route
+        for i in range(len(p)):
+            last_exit_time = current_time
+            
+            current_market = p[i]
+            opening_time = markets[current_market, opening_loc]
+            closing_time = markets[current_market, closing_loc]
+            
+            if i > 0:
+                # walk to next destination
+                last_market = p[i-1]
+                walking_time = durations[last_market, current_market]
+                current_time += walking_time
+            
+            # wait until market is opening
+            if current_time < opening_time:
+                current_time = opening_time
+
+            if current_time >= closing_time:
+                current_time = last_exit_time
+                break
+            
+            # stay for 30 mins or until market is closing
+            current_time += TIME_PER_MARKET
+            if current_time > closing_time:
+                if NEED_FULL_STAY_AT_LAST_MARKET:
+                    break
+                else:
+                    current_time = closing_time
+                    markets_visited += 1
+                    break
+                
+            markets_visited += 1
+        
+        end_time = current_time
+        
+        time_wasted = (last_closing_time - first_opening_time) - TIME_PER_MARKET * markets_visited
+        
+        return markets_visited, time_wasted
+
+    def create_solver() -> GA_TSP:
+        print("create_solver()")
+        ga = GA_TSP(func=lambda p: christmas_market(p)[1], n_dim=len(markets), size_pop=50, prob_mut=1)
+        return ga
+
+    pool = ObjectPool(create_solver)
     
-    # walk along route
-    for i in range(len(p)):
-        last_exit_time = current_time
+    # run algorithm with different number of iterations
+    max_iters = np.geomspace(1, 1000, num=5, dtype=np.int32)
+    markets_visited = np.zeros_like(max_iters, dtype=np.float32)
+    time_wasted = np.zeros_like(max_iters, dtype=np.float32)
+    num_runs_per_iter = np.full_like(max_iters, 5)
+
+    def perform_runs(max_iter: int, num_runs: int) -> Tuple[float, float]:
+        avg_markets_visited = 0.0
+        avg_time_wasted = 0.0
+
+        with pool.item() as ga:
+            for i in range(num_runs):
+                print(f"Iterations: {max_iter}, Run {i+1}/{num_runs}")
+                route, _ = ga.run(max_iter)
+                current_market_visited, current_time_wasted = christmas_market(route)
+                avg_markets_visited += current_market_visited
+                avg_time_wasted += current_time_wasted
+            
+        avg_markets_visited /= num_runs
+        avg_time_wasted /= num_runs
         
-        current_market = p[i]
-        opening_time = markets[current_market, opening_loc]
-        closing_time = markets[current_market, closing_loc]
+        return (avg_markets_visited, avg_time_wasted)
+
+    import datetime
+    start = datetime.datetime.now()
+    # with Pool(5) as executor:
+    #     results = executor.map(perform_runs, zip(max_iters, num_runs_per_iter))
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+    #     results = executor.map(perform_runs, max_iters, num_runs_per_iter)
+    results = map(perform_runs, max_iters, num_runs_per_iter)
+    results = list(results)
+    end = datetime.datetime.now()
+    print(end-start)
+    best = max(results, key=lambda x: x[0])
+    markets_visited = [t[0] for t in results]
+    time_wasted = [t[1] for t in results]
+
+    # save results to csv file
+    df = pd.DataFrame(np.transpose(np.vstack((max_iters, markets_visited, time_wasted))), columns=[
+                    "Iterations", "Markets visited", "Time wasted"])
+    df.to_csv(os.path.join("data", "runs.csv"), header=True, index=False)
+
+
+    # plot results
+    fig, ax1 = plt.subplots()
+    ax1.set_title("Time-constrained travelling Christmas market visitor")
+    # fig.tight_layout()
+
+    ax1.set_xlabel("Iterations")
+    ax1.set_ylabel("Markets visited")
+    ax1.semilogx(max_iters, markets_visited)
+
+    # ax2 = ax1.twinx()
+    # ax2.set_xlabel("Iterations")
+    # ax2.set_ylabel("Time wasted")
+    # ax2.semilogx(max_iters, time_wasted)
+
+    plt.show()
+
+
+    # print best route
+    best_route = best[1]
+    print(f"Best route: {best_route}")
+    assert(np.unique(best_route).shape == best_route.shape)
+
+    current_time = markets[best_route[0], opening_loc]
+    print(f"1. {format_time(current_time)} - {markets[best_route[0], name_loc]}")
+    for i in range(1, len(best_route)):
+        opening_time = markets[best_route[i], opening_loc]
+        closing_time = markets[best_route[i], closing_loc]
         
-        if i > 0:
-            # walk to next destination
-            last_market = p[i-1]
-            walking_time = durations[last_market, current_market]
-            current_time += walking_time
+        walking_time = durations[best_route[i - 1], best_route[i]]
         
-        # wait until market is opening
+        current_time += walking_time
+        
         if current_time < opening_time:
             current_time = opening_time
 
-        if current_time >= closing_time:
-            current_time = last_exit_time
+        if current_time > closing_time:
+            break
+
+        if current_time + TIME_PER_MARKET > closing_time and NEED_FULL_STAY_AT_LAST_MARKET:
             break
         
-        # stay for 30 mins or until market is closing
+        print(f"\tWalking {format_duration(walking_time)}")
+        print(
+            f"{i+1}. {format_time(current_time)} - {markets[best_route[i], name_loc]}")
+
         current_time += TIME_PER_MARKET
+        
         if current_time > closing_time:
-            if NEED_FULL_STAY_AT_LAST_MARKET:
-                break
-            else:
-                current_time = closing_time
-                markets_visited += 1
-                break
-            
-        markets_visited += 1
-    
-    end_time = current_time
-    
-    time_wasted = (last_closing_time - first_opening_time) - TIME_PER_MARKET * markets_visited
-    
-    return markets_visited, time_wasted
+            break
 
-
-# define genetic algorithm
-ga = GA_TSP(func=lambda p: christmas_market(p)[1], n_dim=len(markets), size_pop=50, prob_mut=1)
-
-
-# run algorithm with different number of iterations
-max_iters = np.geomspace(1, 4000, num=50, dtype=np.int32)
-markets_visited = np.zeros_like(max_iters, dtype=np.int32)
-
-best = (0, [])
-for i, max_iter in enumerate(max_iters):
-    print(max_iter)
-    route, _ = ga.run(max_iter)
-    markets_visited[i] = christmas_market(route)[0]
-    if markets_visited[i] > best[0]:
-        best = (markets_visited[i], route)
-
-
-# save results to csv file
-df = pd.DataFrame(np.transpose(np.vstack((max_iters, markets_visited))), columns=[
-                  "Iterations", "Markets visited"])
-df.to_csv(os.path.join("data", "runs.csv"), header=True, index=False)
-
-
-# plot results
-plt.figure()
-plt.semilogx(max_iters, markets_visited)
-plt.title("Time-constrained travelling Christmas market visitor")
-plt.xlabel("Iterations")
-plt.ylabel("Markets visited")
-plt.show()
-
-
-# print best route
-best_route = best[1]
-print(f"Best route: {best_route}")
-assert(np.unique(best_route).shape == best_route.shape)
-
-current_time = markets[best_route[0], opening_loc]
-print(f"1. {format_time(current_time)} - {markets[best_route[0], name_loc]}")
-for i in range(1, len(best_route)):
-    opening_time = markets[best_route[i], opening_loc]
-    closing_time = markets[best_route[i], closing_loc]
-    
-    walking_time = durations[best_route[i - 1], best_route[i]]
-    
-    current_time += walking_time
-    
-    if current_time < opening_time:
-        current_time = opening_time
-
-    if current_time > closing_time:
-        break
-
-    if current_time + TIME_PER_MARKET > closing_time and NEED_FULL_STAY_AT_LAST_MARKET:
-        break
-    
-    print(f"\tWalking {format_duration(walking_time)}")
-    print(
-        f"{i+1}. {format_time(current_time)} - {markets[best_route[i], name_loc]}")
-
-    current_time += TIME_PER_MARKET
-    
-    if current_time > closing_time:
-        break
-
+if __name__ == '__main__':
+    main()
