d6ff41220a5b4d8ffb115763312a526d7c09bd49
Martin <e11909911@student.tuwien.ac.at>
2022-11-27 21:21:05 +0100

    Add matrix run for mutation operators and rates

--- GA/christmas_market.py
+++ GA/christmas_market.py
@@ -6,6 +6,9 @@ import threading
 from time import sleep
 from typing import Optional, Sequence, Tuple
 from sko.GA import GA_TSP
+import sko
+from sko import operators
+from sko.operators import mutation, selection
 from nptime import nptime as time
 from datetime import timedelta
 import pandas as pd
@@ -65,13 +68,19 @@ def load_distance_matrix(path: PathLike) -> pd.DataFrame:
 
 @dataclass
 class PerformRunsResult:
+    markets_visited_per_generation: Sequence[int]
     best_route_markets_visited: int
-    best_route_time_wasted: float
     best_route: Sequence[int]
     runtime: float
+    max_iter: int
+    pop_size: int
+    mutation_rate: float
+    mutation_operator_name: str
 
 
-def perform_runs(markets, durations, max_iter: int, pop_size: int) -> PerformRunsResult:
+def perform_runs(markets, durations, max_iter: int, pop_size: int, mutation_operator_name: str, mutation_rate: float, selection_operator_name: str) -> PerformRunsResult:
+    print(markets.shape, durations.shape, max_iter, pop_size, mutation_operator_name, mutation_rate, selection_operator_name)
+    
     first_opening_time = min(markets[:, OPENING_INDEX])
     last_closing_time = max(markets[:, CLOSING_INDEX])
 
@@ -122,24 +131,51 @@ def perform_runs(markets, durations, max_iter: int, pop_size: int) -> PerformRun
         return markets_visited, time_wasted
 
     best_route_markets_visited = 0
-    best_route_time_wasted = 0
     best_route = []
 
     ga = GA_TSP(func=lambda p: christmas_market(
-        p)[1], n_dim=len(markets), size_pop=pop_size, prob_mut=1)
+        p)[1], n_dim=len(markets), size_pop=pop_size, prob_mut=mutation_rate)
+    
+    if mutation_operator_name == "mutation":
+        mutation_operator = mutation.mutation
+    elif mutation_operator_name == "mutation_TSP_1":
+        mutation_operator = mutation.mutation_TSP_1
+    elif mutation_operator_name == "mutation_reverse":
+        mutation_operator = mutation.mutation_reverse
+    elif mutation_operator_name == "mutation_swap":
+        mutation_operator = mutation.mutation_swap
+    ga.register("mutation", mutation_operator)
+
+    if selection_operator_name == "tournament-3":
+        selection_operator = lambda x: selection.selection_tournament_faster(x, 3)
+    elif selection_operator_name == "tournament-5":
+        selection_operator = lambda x: selection.selection_tournament_faster(x, 5)
+    elif selection_operator_name == "roulette-2":
+        selection_operator = selection.selection_roulette_1
+    elif selection_operator_name == "roulette-2":
+        selection_operator = selection.selection_roulette_2
+    ga.register("selection", selection_operator)
+    
     start = datetime.datetime.now()
     route, _ = ga.run(max_iter)
     end = datetime.datetime.now()
-    current_markets_visited, current_time_wasted = christmas_market(
-        route)
-    if current_markets_visited > best_route_markets_visited:
-        best_route_markets_visited = current_markets_visited
-        best_route_time_wasted = current_time_wasted
-        best_route = route
+    
+    route_per_generation = ga.generation_best_X
+    markets_visited_per_generation = np.zeros((max_iter), dtype=np.int32)
+    best_route_markets_visited = 0
+    best_route = []
+    for iter in range(1, max_iter+1):
+        current_route = route_per_generation[iter-1]
+        current_markets_visited = christmas_market(current_route)[0]
+        markets_visited_per_generation[iter-1] = current_markets_visited
+        if current_markets_visited > best_route_markets_visited:
+            best_route_markets_visited = current_markets_visited
+            best_route = route
 
     # TODO note: the runtime would break if used like this for multiple runs
-    return PerformRunsResult(best_route_markets_visited,
-                             best_route_time_wasted, best_route, (end - start).total_seconds())
+    return PerformRunsResult(markets_visited_per_generation, best_route_markets_visited,
+                             best_route, (end - start).total_seconds(), max_iter, pop_size,
+                             mutation_rate, mutation_operator_name)
 
 
 def print_route(route, markets, durations):
@@ -207,68 +243,79 @@ def run(dataset_name: str, market_data_path: PathLike, durations_data_path: Path
 
     # run algorithm with different number of iterations
     num_runs_per_iter = 10
-    max_iters = np.repeat(np.unique(np.geomspace(1, 4000, num=50, dtype=np.int32)), num_runs_per_iter)
+    max_iter = 4000
+    pop_size = 200
+    mutation_operators = ["mutation_TSP_1", "mutation_reverse", "mutation_swap"]
+    mutation_rates = [0.0001, 0.001, 0.01, 0.1, 1]
+    selection_operators = ["tournament-3", "tournament-5",
+                           "roulette-1", "roulette-2"]
+    
+    runs = list()
+    # for op in mutation_operators:
+    #     for rate in mutation_rates:
+    #         for i in range(num_runs_per_iter):
+    #             runs.append((markets, durations, max_iter, pop_size, op, rate, "tournament-3"))
+    for op in selection_operators:
+        for i in range(num_runs_per_iter):
+            runs.append((markets, durations, max_iter, pop_size, "mutation_reverse", 1, op))
+    
+    start = datetime.datetime.now()
+    with Pool(os.cpu_count() - 1, init_worker) as executor:
+        results = executor.starmap_async(perform_runs, runs, chunksize=1)
+        try:
+            while not results.ready():
+                sleep(0.5)
+            results = list(results.get())
+        except KeyboardInterrupt:
+            executor.terminate()
+            executor.join()
+            exit()
+        else:
+            executor.close()
+            executor.join()
 
-    result_array = np.empty((0, 4))
-    for pop_size in [50, 100, 150, 200]:
-        start = datetime.datetime.now()
-        with Pool(os.cpu_count() - 1, init_worker) as executor:
-            results = executor.starmap_async(perform_runs, zip(
-                np.full((max_iters.shape[0], markets.shape[0],
-                         markets.shape[1]), markets, dtype="object"),
-                np.full((max_iters.shape[0], durations.shape[0],
-                         durations.shape[1]), durations, dtype="object"),
-                reversed(max_iters),
-                np.full_like(max_iters, pop_size)
-            ), chunksize=1)
-            try:
-                while not results.ready():
-                    sleep(0.5)
-            except KeyboardInterrupt:
-                executor.terminate()
-                executor.join()
-                exit()
-            else:
-                executor.close()
-                executor.join()
+    end = datetime.datetime.now()
+    print(f"Ran for {format_duration((end - start).seconds)}")
 
-        # with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        #     results = executor.map(perform_runs, max_iters, num_runs_per_iter)
-        # results = map(perform_runs, max_iters, num_runs_per_iter)
-        results = list(reversed(list(results.get())))
+    # Iterations,Markets visited,runtimes,population_size
+    res = list()
+    for r in results:
+        for iter in range(r.max_iter):
+            res.append((iter+1, r.markets_visited_per_generation[iter], r.runtime, r.pop_size,
+                        r.mutation_operator_name, r.mutation_rate, r.selection_operator_name))
+    df = pd.DataFrame(res, columns=(
+        "Iterations", "Markets visited", "Runtime", "Population size", "Mutation operator", "Mutation rate", "Selection operator"))
+    df.to_csv(os.path.join("data", "runs",
+              f"{dataset_name}_runs3.csv"), header=True, index=False)
 
-        end = datetime.datetime.now()
-        print(f"Ran for {format_duration((end - start).seconds)}")
+    # markets_visited = np.array([t.best_route_markets_visited for t in results])
+    # runtimes = np.array([t.runtime for t in results])
+    # result_array = np.vstack((result_array, np.transpose(np.vstack((max_iters, markets_visited, runtimes,
+    #                                                                 np.repeat(pop_size, len(results)))))))
 
-        best = max(results, key=lambda x: x.best_route_markets_visited)
-        markets_visited = np.array([t.best_route_markets_visited for t in results])
-        runtimes = np.array([t.runtime for t in results])
-        result_array = np.vstack((result_array, np.transpose(np.vstack((max_iters, markets_visited, runtimes,
-                                                                        np.repeat(pop_size, len(results)))))))
+    # if show_plot_runs or save_plot_runs:
+    #     figure_path = None
+    #     if save_plot_runs:
+    #         os.makedirs(os.path.join("data", "figures"), exist_ok=True)
+    #         figure_path = os.path.join("data", "figures", f"{dataset_name}_figure.png")
 
-        if show_plot_runs or save_plot_runs:
-            figure_path = None
-            if save_plot_runs:
-                os.makedirs(os.path.join("data", "figures"), exist_ok=True)
-                figure_path = os.path.join("data", "figures", f"{dataset_name}_figure.png")
+    #     # plot best visited markets against iterations
+    #     plot_runs(max_iters, markets_visited, show_plot_runs, figure_path)
 
-            # plot best visited markets against iterations
-            plot_runs(max_iters, markets_visited, show_plot_runs, figure_path)
+    # if show_route:
+    #     # print best route
+    #     best_route = best.best_route
+    #     assert (np.unique(best_route).shape == best_route.shape)
+    #     print(f"Best route visites {np.ceil(best_route.shape[0])} markets")
+    #     print_route(best_route, markets, durations)
 
-        if show_route:
-            # print best route
-            best_route = best.best_route
-            assert (np.unique(best_route).shape == best_route.shape)
-            print(f"Best route visites {np.ceil(best_route.shape[0])} markets")
-            print_route(best_route, markets, durations)
+    # if save_runs:
+    #     # save results to csv file
 
-    if save_runs:
-        # save results to csv file
-
-        df = pd.DataFrame(result_array, columns=[
-            "Iterations", "Markets visited", "runtimes", "population_size"])
-        os.makedirs(os.path.join("data", "runs"), exist_ok=True)
-        df.to_csv(os.path.join("data", "runs", f"{dataset_name}_runs.csv"), header=True, index=False)
+    #     df = pd.DataFrame(result_array, columns=[
+    #         "Iterations", "Markets visited", "runtimes", "population_size"])
+    #     os.makedirs(os.path.join("data", "runs"), exist_ok=True)
+    #     df.to_csv(os.path.join("data", "runs", f"{dataset_name}_runs.csv"), header=True, index=False)
 
 
 def main_single():
