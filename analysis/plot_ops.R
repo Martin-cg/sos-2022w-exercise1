9e6d760da0fab2246d84d430d6ae75ded9318d1b
Peter Blohm <e11905150@student.tuwien.ac.at>
2022-11-27 22:27:21 +0100

    Fix legend mistake

--- analysis/plot_ops.R
+++ analysis/plot_ops.R
@@ -7,7 +7,7 @@ data2 <- read.csv("data/runs/tcmt_default_runs2.csv")
 
 plt2 <- ggplot(data2, aes(x=Iterations, y=Markets.visited, col=as.factor(Mutation.rate))) +
     facet_grid(Mutation.operator~.) +
-    scale_color_discrete(name = "Mutation operator") +
+    scale_color_discrete(name = "Mutation rate") +
     scale_y_continuous(breaks=pretty_breaks()) +
     # geom_jitter() +
     geom_smooth() +
@@ -27,5 +27,5 @@ plt3 <- ggplot(data3, aes(x=Iterations, y=Markets.visited, col=Selection.operato
     # geom_jitter() +
     geom_smooth() +
     labs(title = "GA average solution quality with different selection operator", y = "markets visited")
-ggsave("data/figures/runs2_ggplot.png", plt3)
+ggsave("data/figures/runs3_ggplot.png", plt3)
 print(plt3)
